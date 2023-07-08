a66b96a468937d8bd3b64a8bcf993b4b44c15021
Abdullah Mirza <111111588+DevMirxa009@users.noreply.github.com>
2023-07-08 12:05:42 +0500

    day one code simple input and concatenate strings

---
+++ day1-Band-Name-Generator/main.py
@@ -0,0 +1,7 @@
+print('Wellcome to my BandName generator \n')
+city=input("In which city do you live? ")
+pet=input("\n what is your pet name? ")
+
+bandName=pet+ " " +city
+
+print("your band name should be "+bandName )
